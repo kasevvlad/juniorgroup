66591c15a7f601834f8520be6c3d1615e007b388
hamitovdaniil <hamitovdaniil6@gmail.com>
2022-06-06 22:45:02 +0500

    delete lorem

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ $page = 'index';
 include "partials/header.php" ?>
 
 <main>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis cum aliquid eligendi qui magnam repellendus tempore facilis consequuntur temporibus recusandae.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae ab accusamus tempora nemo, officiis voluptatibus aperiam vero delectus blanditiis velit neque, cupiditate eius, omnis quidem repudiandae! Culpa hic reiciendis ea, iste at voluptas suscipit perspiciatis molestiae quis. Accusantium dolor quam architecto, corporis molestiae ad sed nobis perferendis molestias delectus animi perspiciatis est harum, dolorem iure dicta excepturi soluta? Ipsum culpa hic ex minima exercitationem iusto perspiciatis veniam quae, omnis officiis itaque sequi nesciunt, excepturi ut harum architecto inventore delectus, quod rerum eum necessitatibus illum. Libero nihil iure ut eveniet modi, quia distinctio earum rerum aspernatur! Porro vero recusandae quisquam?
+    
 </main>
 
 <?php include "partials/footer.php" ?>
